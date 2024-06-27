fe8d3aba0cc885bfcf082a15ec6eaef8ab1cc1c5
Maryame44 <162489563+Maryame44@users.noreply.github.com>
2024-06-27 14:51:37 +0100

    Update function.py

--- function.py
+++ function.py
@@ -3,14 +3,28 @@ import json
 import boto3
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
+from botocore.exceptions import ClientError
 import base64
 
-def decrypt_env_var(encrypted_value):
-    kms_client = boto3.client('kms')
-    decrypted_value = kms_client.decrypt(
-        CiphertextBlob=base64.b64decode(encrypted_value)
-    )['Plaintext'].decode('utf-8')
-    return decrypted_value
+def decrypt(ciphertext):
+    kms = boto3.client('kms')
+    try:
+        response = kms.decrypt(CiphertextBlob=base64.b64decode(ciphertext))
+        return response['Plaintext'].decode('utf-8')
+    except ClientError as e:
+        print(f"Erreur lors du déchiffrement: {e}")
+        return None
+
+def get_secret(secret_name):
+    client = boto3.client('secretsmanager')
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        print(f"Erreur lors de la récupération du secret: {e}")
+        return None
+
+    secret = get_secret_value_response['SecretString']
+    return json.loads(secret)
 
 def create_drive_service(client_id, client_secret, refresh_token):
     creds = Credentials(
@@ -24,16 +38,36 @@ def create_drive_service(client_id, client_secret, refresh_token):
     return drive_service
 
 def lambda_handler(event, context):
-    # Récupérer les informations de connexion depuis les variables d'environnement chiffrées
-    encrypted_client_id = os.environ['ENCRYPTED_CLIENT_ID']
-    encrypted_client_secret = os.environ['ENCRYPTED_CLIENT_SECRET']
-    encrypted_refresh_token = os.environ['ENCRYPTED_REFRESH_TOKEN']
-    
-    try:
-        client_id = decrypt_env_var(encrypted_client_id)
-        client_secret = decrypt_env_var(encrypted_client_secret)
-        refresh_token = decrypt_env_var(encrypted_refresh_token)
+    secret_name = "my-google-drive-secret"  #  le nom de  secret
 
+    secrets = get_secret(secret_name)
+    if not secrets:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Erreur lors de la récupération des secrets.')
+        }
+
+    encrypted_client_id = secrets.get('ENCRYPTED_CLIENT_ID')
+    encrypted_client_secret = secrets.get('ENCRYPTED_CLIENT_SECRET')
+    encrypted_refresh_token = secrets.get('ENCRYPTED_REFRESH_TOKEN')
+
+    if not encrypted_client_id or not encrypted_client_secret or not encrypted_refresh_token:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Les informations d\'identification chiffrées sont requises.')
+        }
+
+    client_id = decrypt(encrypted_client_id)
+    client_secret = decrypt(encrypted_client_secret)
+    refresh_token = decrypt(encrypted_refresh_token)
+    
+    if not client_id or not client_secret or not refresh_token:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Erreur lors du déchiffrement des informations d\'identification.')
+        }
+
+    try:
         drive_service = create_drive_service(client_id, client_secret, refresh_token)
         
         # Récupérer la liste des fichiers du Google Drive
